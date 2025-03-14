7a37dcdace24a5e68db544333614615dcb3037bc
Francesco Ilario <filario@redhat.com>
2025-03-14 11:07:37 +0100

    fix name too long
    max length for kubernetes object name is 63
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -37,7 +37,7 @@ const namespace = "alert-manager-kueue-admission-system"
 const serviceAccountName = "alert-manager-kueue-admission-controller-manager"
 
 // metricsServiceName is the name of the metrics service of the project
-const metricsServiceName = "alert-manager-kueue-admission-controller-manager-metrics-service"
+const metricsServiceName = "alert-mgr-kueue-admission-controller-manager-metrics-service"
 
 // metricsRoleBindingName is the name of the RBAC that will be created to allow get the metrics data
 const metricsRoleBindingName = "alert-manager-kueue-admission-metrics-binding"
